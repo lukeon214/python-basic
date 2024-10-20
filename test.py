07bbc26a6e4cceed49bffa6e194139339b229ddb
lukeon <163727888+lukeon214@users.noreply.github.com>
2024-10-20 16:10:19 +0000

    some stuff

---
+++ test.py
@@ -0,0 +1,11 @@
+print("Let's do  some simple meth")
+
+# multiple inputs
+num1 = int(input("Enter your first number "))
+num2 = int(input("Enter your second number "))
+
+# result calculating
+result = num1 + num2
+
+# printing
+print("After you add up these two numbers this comes off:", result)
