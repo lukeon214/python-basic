f0dca994c83babaa8816130e1f0a7989ac0aa7b4
lukeon <163727888+lukeon214@users.noreply.github.com>
2024-10-20 19:15:35 +0200

    Choices

---
+++ choices.py
@@ -0,0 +1,17 @@
+print("Choose between 1-3")
+print("1")
+print("2")
+print("3")
+
+choice = int(input("Input a number: "))
+
+if choice == 1:
+    print("You chose number 1")
+elif choice == 2:
+    print("You chose number 2")
+elif choice == 3:
+    print("You chose number 3")
+
+else: 
+    print("Invelid number inputed")
+    
